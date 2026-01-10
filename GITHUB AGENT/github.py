62ea745b40a924c9883bd660fb206c56a231c5fd
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2026-01-10 16:29:25 +0530

    github.py

---
+++ GITHUB AGENT/github.py
@@ -0,0 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+from langchain_core.agents import Document
+
+load_dotenv()
+github_token = os.getenv("GITHUB_TOKEN")
+
+def fetch_github(owner, repo, endpoint):
+    url = f"https://api.github.com/repos/{owner}/{repo}/{endpoint}"  #made it dynamic so can load any type of repo
+    headers = {
+        "Authorization": f"Bearer {github_token}"
+    }
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print("Failed with status code:", response.status_code)
+        return[]
+    
+    print(data)
+    return data
+
+owner = "aroraadivya"
+repo = "AI-agent"
+endpoint = "issues"  # Example endpoint to fetch issues
+fetch_github(owner, repo, endpoint)
